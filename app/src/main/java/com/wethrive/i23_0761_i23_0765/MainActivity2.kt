709941ce988b41426ac79e73d9df2ba97e149fd7
Hudiyahaha <hudiyahashim@gmail.com>
2025-10-11 11:04:51 +0500

    app running with userdata

--- app/src/main/java/com/wethrive/i23_0761_i23_0765/MainActivity2.kt
+++ app/src/main/java/com/wethrive/i23_0761_i23_0765/MainActivity2.kt
@@ -6,26 +6,27 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity2 : AppCompatActivity() {
-    lateinit var mAuth: FirebaseAuth
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         mAuth= FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
         setContentView(R.layout.activity_main2)
-        var name=findViewById<EditText>(R.id.username)
-        var email=findViewById<EditText>(R.id.email)
-        var pass=findViewById<EditText>(R.id.pass)
-        var signup=findViewById<Button>(R.id.signup)
+        val name=findViewById<EditText>(R.id.username)
+        val email=findViewById<EditText>(R.id.email)
+        val pass=findViewById<EditText>(R.id.pass)
+        val signup=findViewById<Button>(R.id.signup)
         signup.setOnClickListener {
-            var em=email.text.toString()
-            var pa=pass.text.toString()
-            var uname=name.text.toString()
+            val em=email.text.toString()
+            val pa=pass.text.toString()
+            val uname=name.text.toString()
             if (em.isEmpty() || pa.isEmpty()) {
                 email.error = "Email required"
                 pass.error = "Password required"
@@ -37,14 +38,17 @@ class MainActivity2 : AppCompatActivity() {
             mAuth.createUserWithEmailAndPassword(em,pa)
                 .addOnCompleteListener {
                     if(it.isSuccessful){
-                        val uid = FirebaseAuth.getInstance().currentUser?.uid
+                        val uid = mAuth.currentUser?.uid
                         val user = UserData(uid, uname, em,pa)
-                        var intent= Intent(this,MainActivity3::class.java)
+                        if (uid != null) {
+                            database.getReference("Users").child(uid).setValue(user)
+                        }
+                        val intent= Intent(this,MainActivity3::class.java)
                         startActivity(intent)
                         finish()
                     }
                     else{
-                        var msg=it.exception?.localizedMessage
+                        val msg=it.exception?.localizedMessage
                         email.error=msg.toString()
                     }
                 }
@@ -58,7 +62,7 @@ class MainActivity2 : AppCompatActivity() {
        super.onStart()
     if (mAuth.currentUser!=null)
         {
-            var i=Intent(this, MainActivity5::class.java)
+            val i=Intent(this, MainActivity5::class.java)
            startActivity(i)
             finish()
         }
---
+++ app/src/main/java/com/wethrive/i23_0761_i23_0765/UserData.kt
@@ -0,0 +1,8 @@
+package com.wethrive.i23_0761_i23_0765
+
+data class UserData(
+    val uid: String? = null,
+    val name: String? = null,
+    val email: String? = null,
+    val pass: String? = null
+)
